f21ed2f3928183ee3134d5ddade7376c79fa4520
dungdang236 <ios31@Mac-mini.local>
2016-01-14 11:36:36 +0700

    abc

--- exampleContentAndBounce/ViewController.m
+++ exampleContentAndBounce/ViewController.m
@@ -9,19 +9,32 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIScrollView * scrollView;
 @end
 
 @implementation ViewController
-
+{
+    UIImageView* photo;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.view.backgroundColor = [UIColor whiteColor];
+    UIImage* image = [UIImage imageNamed:@"people.jpg"];
+    self.scrollView= [[UIScrollView alloc] initWithFrame:CGRectMake(8, 8, self.view.bounds.size.width-16, 300)];
+    self.scrollView.backgroundColor= [UIColor grayColor];
+    self.scrollView.contentSize= CGSizeMake(image.size.width, image.size.height);
+    
+    self.scrollView.bounces= true;
+    self.scrollView.showsHorizontalScrollIndicator= true;
+    self.scrollView.showsVerticalScrollIndicator= true;
+    
+    photo = [[UIImageView alloc] initWithImage:image];
+    photo.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    [self.scrollView addSubview:photo];
+    [self.view addSubview:self.scrollView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
